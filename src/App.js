b8db7f76e9c8a4d415e5c6313ebbb5c3bd5605df
Мальцева Любовь <rikyhollundead1234@gmail.com>
2022-11-08 18:57:46 +0500

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import AddTask from "./Components/Addtask";
+import ListTasks from "./Components/ListTasks";
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 0,
+      text: 'Купить хлеб',
+      checked: false
+    },
+    {
+      id: 1,
+      text: 'Сделать уборку',
+      checked: false
+    }
+  ]);
+
+  // const [deletedTasks, setDeletedTasks] = useState([]);
+
+  console.log(tasks);
+  // console.log('test' + deletedTasks);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="intro">
+      <h2 className="intro_title"> Мой список задач</h2>
+
+      <ListTasks 
+        tasks={tasks} setTasks={setTasks}
+      />
+      <AddTask 
+        tasks={tasks} setTasks={setTasks}
+      />
     </div>
   );
 }
